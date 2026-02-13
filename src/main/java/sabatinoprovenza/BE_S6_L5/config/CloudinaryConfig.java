63b6630ba07cfc5cf0634ebf83203ab75efe0064
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-02-13 14:23:34 +0100

    Add Cloudinary dependency and config

---
+++ src/main/java/sabatinoprovenza/BE_S6_L5/config/CloudinaryConfig.java
@@ -0,0 +1,27 @@
+package sabatinoprovenza.BE_S6_L5.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary(@Value("${cloudinary.name}") String name,
+                                 @Value("${cloudinary.api_key}") String apiKey,
+                                 @Value("${cloudinary.api_secret}") String apiSecret) {
+
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", name);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+
+        return new Cloudinary(config);
+    }
+}
+
